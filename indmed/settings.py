774bf5f38f1dd03bc8abdba794265fdb11fb61a6
shray3 <shrayk3@gmail.com>
2015-04-28 15:05:20 -0400

    Update settings.py

--- indmed/settings.py
+++ indmed/settings.py
@@ -92,13 +92,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = 'polls/static/'
-STATIC_ROOT = "statics/"
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
-
 
 # Parse database configuration from $DATABASE_URL
 import dj_database_url

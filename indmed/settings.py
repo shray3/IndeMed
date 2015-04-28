6df82fa573b4516c34a1d7f060679c28157ce89f
shray3 <shrayk3@gmail.com>
2015-04-28 14:17:44 -0400

    Update settings.py

--- indmed/settings.py
+++ indmed/settings.py
@@ -93,4 +93,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = 'polls/static/'
-STATIC_ROOT = "statics/"
+STATIC_ROOT = 'staticfiles'
+
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
+
+# Static asset configuration
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
